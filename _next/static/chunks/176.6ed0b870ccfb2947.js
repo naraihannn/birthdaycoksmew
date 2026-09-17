a93b2d8b35f9c958b04a43476bd226f6c16640df
Naufal Raihan <67577583+naraihannn@users.noreply.github.com>
2026-09-17 19:54:00 +0700

    Update 176.6ed0b870ccfb2947.js

--- _next/static/chunks/176.6ed0b870ccfb2947.js
+++ _next/static/chunks/176.6ed0b870ccfb2947.js
@@ -1117,7 +1117,7 @@
                         }
                     }, []);
                     let W = Array.from({
-                            length: 12
+                            length: 14
                         }, (e, t)=>"/birthdaycoksmew/images/foto".concat(t+1,".jpg")),
                         [H, G] = (0, a.useState)(new Date);
                     (0, a.useEffect)(() => {
